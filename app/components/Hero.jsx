5a5168ce4057258077377d5f86ddc4c37f11e8ea
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2025-03-16 10:41:01 +0100

    Hero Section

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -5,13 +5,19 @@ const Hero = () => {
         <>
             <section className="relative h-screen bg-[url('/text.jpg')] bg-center bg-cover">
                 {/* Gradient Overlay */}
-                <div className="absolute inset-0 bg-gradient-to-b from-black/70 via-black/50 to-transparent"></div>
+                <div className="absolute inset-0 bg-gradient-to-b from-black/90 via-black/70 to-transparent"></div>
+                <div className="absolute inset-0 bg-gradient-to-t from-black/90 via-black/70 to-transparent"></div>
 
                 {/* Content */}
-                <div className="relative z-10 flex items-center justify-center h-full text-white text-center">
-                    <h1 className="text-5xl font-bold">Leadership with Vision, <span className="text-green-500">Service</span> with Integrity</h1>
+                <div className="relative z-10 flex flex-col items-center justify-center h-full text-white text-center px-6">
+                    <h2 className="text-lg sm:text-2xl font-light uppercase tracking-wider">Welcome to the official website of</h2>
+                    <h1 className="text-4xl sm:text-6xl font-extrabold mt-4">
+                        Muhammad <span className="text-green-500">Kabade</span> Suleiman
+                    </h1>
+                    <p className='mt-4'>National Youth Leader of The PDP <span className='font-bold'>(MON)</span></p>
                 </div>
             </section>
+
 
         </>
     )
